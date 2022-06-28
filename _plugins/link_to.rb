d4a6889bdd1ffc31d6b2dfb07c71f86cc75eecc6
alnf <alnf@users.noreply.github.com>
2022-06-28 15:59:26 +0000

    Tue Jun 28 15:59:26 UTC 2022

--- _plugins/link_to.rb
+++
@@ -1,9 +0,0 @@
-# Custom Liquid filter to create hyperlinks
-# {{ 'href' | link_to: 'input' }}
-module Jekyll::CustomFilter
-  def link_to(input, href)
-    "<a href=\"#{href}\">#{input}</a>"
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::CustomFilter)
